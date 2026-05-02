55a01316c800cbfc816936675f03dea94a4f91c8
yhb <fhtmxm2303@gmail.com>
2026-05-02 12:17:07 +0900

    Initial Commit

---
+++ d-day-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  d-day-app
+//
+//  Created by 정연희 on 2026/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ d-day-app/d_day_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  d_day_appApp.swift
+//  d-day-app
+//
+//  Created by 정연희 on 2026/05/02.
+//
+
+import SwiftUI
+
+@main
+struct d_day_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
